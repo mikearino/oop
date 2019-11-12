2d09cd9f17790b654b5fe37666e797afa8f53057
Mike Lofgren <mikeylofgren@gmail.com>
2019-11-11 21:24:44 -0800

    class inheritance

--- objects.js
+++ objects.js
@@ -1,12 +1,35 @@
-var userOne = {
-  email: "ryu@ninjas.com",
-  name: "Ryu",
-  login() {
-    console.log(this.email, "has logged in");
-  },
-  logout() {
-    console.log(this.email, "has logged out");
+class User {
+  constructor(email, name) {
+    this.email = email;
+    this.name = name;
+    this.score = 0;
   }
-};
+  login() {
+    console.log(this.email, "just logged in");
+    return this;
+  }
+  logout() {
+    console.log(this.email, "just logged out");
+    return this;
+  }
+  updateScore() {
+    this.score++;
+    console.log(this.email, this.name, "score is now", this.score);
+    return this;
+  }
+}
 
-console.log(userOne.name);
+class Admin extends User {
+  deleteUser(user) {
+    users = users.filter(u => {
+      return u.email != user.email;
+    });
+  }
+}
+
+var userOne = new User("ryu@ninja.com", "Ryu");
+var userTwo = new User("Yoshi@marioKorp.com", "Yoshi");
+var admin = new Admin("peach@marioKart", "Peach");
+var users = [userOne, userTwo, admin];
+
+console.log(users);
